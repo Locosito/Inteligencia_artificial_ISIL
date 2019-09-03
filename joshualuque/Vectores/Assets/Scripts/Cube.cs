ae88941264f1609ef63d3dc916245f09d5d43860
Locosito <joshua_luquevilela@hotmail.com>
2019-09-02 21:27:07 -0500

    Clase 2 - Vectores

---
+++ joshualuque/Vectores/Assets/Scripts/Cube.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+
+    Vector3 velocity = new Vector3(3,2,0);
+    Vector3 acceleration = new Vector3(0,-9.8f,0);
+
+    void Update()
+    {
+        transform.position += velocity * Time.deltaTime;
+        velocity += acceleration * Time.deltaTime;
+
+        if(transform.position.x >= 9.41f){
+            velocity.x *= -1;
+        }
+        if(transform.position.x <= -9.49f){
+            velocity.x *= -1;
+        }
+
+        if(transform.position.y < -4.34f){
+            velocity.y *= -1;
+        }
+    }
+}
