7356b376ec6f0df98b3c9c00b47d09c491866b0f
Locosito <joshua_luquevilela@hotmail.com>
2019-09-23 21:27:10 -0500

    Actu clase4

---
+++ joshualuque/Clase4/Assets/Scripts 2/Agent.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Agent : SBAgent
+{
+	public Transform target;
+	public Transform target2;
+
+
+	void Start()
+	{
+		maxSpeed = 10f;
+		maxSteer = 1f;
+
+		target2 = GameObject.Find("Target2").transform;
+	}
+
+	void Update()
+	{
+		velocity += SteeringBehaviours.Arrive(this, target, 3f);
+		velocity += SteeringBehaviours.Flee(this, target2, 5f);
+		velocity += SteeringBehaviours.Separate(this, GameManager.agents, 0.5f);
+		transform.position += velocity * Time.deltaTime;
+	}
+}
